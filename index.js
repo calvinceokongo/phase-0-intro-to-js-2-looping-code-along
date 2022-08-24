cc85220e9a9a0c2702e9b7cbf09ea836e5d10b6a
calvince <calokongo@gmail.com>
2022-08-24 23:43:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+let msg = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        msg.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return msg;
+}
+writeCards(msg)
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(num);
